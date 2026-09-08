3f0ea23c889255ae66972e289fc685cfacd301e8
Brad Howes <bradhowes@mac.com>
2026-09-08 09:22:10 +0200

    New install script and documentation.

--- emacs.d/README.md
+++ emacs.d/README.md
@@ -7,6 +7,19 @@ Current configuration files:
 - [custom.el](custom.el) -- contains customized settings
 - [lisp](lisp) -- directory containing various mode hooks and functions
 
+# Installation
+
+Run the `install.sh` script in this directory. This creates a new `~/.emacs.d` directory and establishes soft-links to the
+files in this repository directory. If `brew` is installed, the script will install packages that the Emacs configuration
+depends on:
+
+- coreutils -- in order to get GNU `ls` (as `gls`) for use in `dired` buffers on macOS
+- ispell -- spelling service used by Emacs
+- shellcheck -- a linter for shell scripts (Bash, Zsh, etc.)
+
+Finally, the script attempts to install GNU Emacs from the `d12frosted/emacs-plus` tap. This installs a pre-built version which
+I'm OK with -- the paranoid may want to view all of the source files and then build with the usual configure steps.
+
 # Historical
 
 Old-school Emacs configuration files kept for posterity. Lots of it come from when I first began using Emacs back in v18
---
+++ emacs.d/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# set -x
+
+function fail()
+{
+  echo "*** ${*}"
+  exit 1
+}
+
+src=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+[[ -d "${src}" ]] || fail "failed to detect source directory"
+
+dst="${HOME}/.emacs.dd"
+if [[ -d "${dst}" ]]; then
+  echo "... '${dst}' already exists"
+else
+  echo "... creating '${dst}'"
+  mkdir "${dst}" || fail "'mkdir ${dst}' failed"
+fi
+
+cd "${dst}" || fail "'cd ${dst}' failed"
+for file in custom.el early-init.el init.el lisp; do
+  if [[ -h "${dst}/${file}" ]]; then
+    echo "... file '${dst}/${file}' already exists"
+  else
+    ln -s "${src}/${file}" "${dst}/${file}" || fail "failed to link to '${file}'"
+  fi
+done
+
+# There are some brew dependencies which would be nice to have
+
+[[ -x "$(type -p brew)" ]] || fail "'brew' not found"
+
+brew install --no-ask coreutils
+brew install --no-ask ispell
+brew install --no-ask shellcheck
+
+# Create tap to d12frosted/emacs-plus and install pre-built binaries
+
+brew tap d12frosted/emacs-plus
+brew install --cask emacs-plus-app
